f510090944bd255e09b33b8bcc8bb4aa2eea09a2
Sieun <sydneysyd0070@naver.com>
2021-11-25 13:05:54 +0900

    12주차 추가

---
+++ app/src/main/java/com/example/myapplication_12st/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.myapplication_12st;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        recyclerView.setLayoutManager(layoutManager);
+        PersonAdapter adapter = new PersonAdapter();
+
+        adapter.addItem(new Person("박시은", "010-1000-1000"));
+        adapter.addItem(new Person("김시은", "010-2000-2000"));
+        adapter.addItem(new Person("이시은", "010-3000-3000"));
+
+        recyclerView.setAdapter(adapter);
+    }
+}
